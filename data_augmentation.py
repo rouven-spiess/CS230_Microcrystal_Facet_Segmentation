84ebb2bf0a1351b29afc8648fdeadab185fd0d12
pan0rama <53146956+pan0rama@users.noreply.github.com>
2020-03-20 00:12:53 +0100

    Create data_augmentation.py

---
+++ data_augmentation.py
@@ -0,0 +1,39 @@
+import Augmentor
+path = "/Users/rouven/Documents/SCPD/CS230/CS230_Project/MAR18/"
+subset = "train" #e.g. "train"
+
+p = Augmentor.Pipeline(path + subset)
+
+# Augmentation parameters
+p.crop_by_size(1, 96, 96, centre=False)
+p.flip_left_right(0.75)
+p.flip_top_bottom(0.75)
+p.rotate_random_90(1)
+p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
+p.shear(0.5, 3, 3)
+p.random_brightness(0.5, 0.8, 1.2)
+p.random_contrast(1.0, 0.5, 3.0)
+p.zoom(1, min_factor=1.0, max_factor=1.6)
+p.random_distortion(0.5, 5, 5, 1)
+p.resize(probability=1.0, width=96, height=96)    
+
+p.ground_truth(path + subset + "_labels")
+
+parameters ="""
+p.crop_by_size(1, 96, 96, centre=False)
+p.flip_left_right(0.75)
+p.flip_top_bottom(0.75)
+p.rotate_random_90(1)
+p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
+p.shear(0.5, 3, 3)
+p.random_brightness(0.5, 0.8, 1.2)
+p.random_contrast(1.0, 0.5, 3.0)
+p.zoom(1, min_factor=1.0, max_factor=1.6)
+p.random_distortion(0.5, 5, 5, 1)
+p.resize(probability=1.0, width=96, height=96)  
+"""
+
+with open(path + "augmentation_" + subset + ".txt", "w") as text_file:
+    text_file.write(parameters)
+    
+p.sample(250)
